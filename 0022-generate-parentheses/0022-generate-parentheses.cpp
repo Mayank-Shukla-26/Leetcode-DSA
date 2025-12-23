3244620b8fbe3ac73a694d1ff6dbb4eeb4d571d9
MAYANK SHUKLA <166242858+Mayank-Shukla-26@users.noreply.github.com>
2025-12-23 23:12:44 +0530

    Time: 3 ms (71.39%), Space: 15.6 MB (62.23%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,27 +1,17 @@
 class Solution {
 public:
-    bool isValid(string s) {
-        int balance = 0;
-        for(char c : s) {
-            if(c == '(') balance++;
-            else balance--;
-            if(balance < 0) return false;
-        }
-        return balance == 0;
-    }
-
-    void generateAll(string curr, int n, vector<string>& res) {
+    void backtrack(string curr, int open, int close, int n, vector<string>& res) {
         if(curr.length() == 2 * n) {
-            if(isValid(curr)) res.push_back(curr);
+            res.push_back(curr);
             return;
         }
-        generateAll(curr + '(', n, res);
-        generateAll(curr + ')', n, res);
+        if(open < n) backtrack(curr + '(', open + 1, close, n, res);
+        if(close < open) backtrack(curr + ')', open, close + 1, n, res);
     }
 
     vector<string> generateParenthesis(int n) {
         vector<string> res;
-        generateAll("", n, res);
+        backtrack("", 0, 0, n, res);
         return res;
     }
 };
