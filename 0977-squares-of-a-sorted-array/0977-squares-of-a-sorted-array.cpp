d1d91165bdebfd0937c6eca081d35a4b16fa87c9
MAYANK SHUKLA <166242858+Mayank-Shukla-26@users.noreply.github.com>
2026-01-26 01:29:10 +0530

    Time: 4 ms (40.96%), Space: 32.8 MB (6.65%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -2,10 +2,38 @@ class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
         vector<int> ans;
+        vector<int> neg;
+        vector<int> pos;
         for(int i=0; i<nums.size(); i++) {
-            ans.push_back(nums[i]*nums[i]);
+            if(nums[i] < 0) {
+                neg.push_back(nums[i] * nums[i]);
+            } else {
+                pos.push_back(nums[i] * nums[i]);
+            }
         }
-        sort(ans.begin(), ans.end());
+
+        reverse(neg.begin(), neg.end());
+        int i = 0;
+        int j = 0;
+
+        while(i < neg.size() && j < pos.size()) {
+            if(neg[i] < pos[j]) {
+                ans.push_back(neg[i]);
+                i++;
+            } else {
+                ans.push_back(pos[j]);
+                j++;
+            }
+        }
+
+        while(i < neg.size()) {
+            ans.push_back(neg[i]);
+            i++;
+        } 
+        while(j < pos.size()) {
+            ans.push_back(pos[j]);
+            j++;
+        }
         return ans;
     }
 };
