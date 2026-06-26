dd1551bc52f3c06dae1c90d37d98aeb2c7384e2d
MAYANK SHUKLA <166242858+Mayank-Shukla-26@users.noreply.github.com>
2026-06-26 21:43:54 +0530

    Time: 0 ms (100%), Space: 28.3 MB (28.75%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1) {
+            return nums[0];
+        }
+
+        sort(nums.begin(), nums.end());
+        int freq = 1;
+
+        for(int i=1; i<n; i++) {
+            if(nums[i] == nums[i-1]) {
+                freq++;
+            } else {
+                freq = 1;
+            }
+            if(freq > n/2) {
+                return nums[i];
+            }
+        }
+
+        return -1;
+    }
+};
