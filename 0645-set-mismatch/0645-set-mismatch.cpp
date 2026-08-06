db0ccd6d5b838189e035f768acaca31da6336cc9
MAYANK SHUKLA <166242858+Mayank-Shukla-26@users.noreply.github.com>
2026-08-07 01:11:50 +0530

    Time: 0 ms (100%), Space: 26.3 MB (37.15%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans;
+        vector<int> freq(n+1, 0);
+
+        for(int i=0; i<n; i++){
+            freq[nums[i]]++;
+        }
+        for(int i=1; i<=n; i++){
+            if(freq[i] == 2) {
+                ans.push_back(i);
+                break;
+            } 
+        }
+        for(int i=1; i<=n; i++) {
+            if(freq[i] == 0) {
+                ans.push_back(i);
+                break;
+            }
+        }
+
+        return ans;
+    }
+};
