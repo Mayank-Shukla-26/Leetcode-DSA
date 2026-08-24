36f63398ba3a25628dabc8bf88faf8922d4184a4
MAYANK SHUKLA <166242858+Mayank-Shukla-26@users.noreply.github.com>
2026-08-24 14:58:00 +0530

    Time: 20 ms (6.64%), Space: 263.1 MB (19.33%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+
+        if(n == 0) return;
+
+        k = k % n;
+        vector<int> ans(n);
+        int j = 0;
+
+        for(int i=n-k; i<n; i++) {
+            ans[j++] = nums[i];
+        }
+
+        for(int i=0; i<n-k; i++) {
+            ans[j++] = nums[i];
+        }
+
+        for(int i=0; i<n; i++) {
+            nums[i] = ans[i];
+        }
+    }
+};
